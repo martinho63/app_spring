3467667b9b92b6b5fe0d4fe08f01a41a04bb1e24
cmart63 <carlosmartinho63@gmail.com>
2024-02-29 18:35:01 -0300

    Create project

---
+++ src/main/java/com/devsuperior/app_spring_aula/AppSpringAulaApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.app_spring_aula;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppSpringAulaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppSpringAulaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devsuperior/app_spring_aula/AppSpringAulaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.app_spring_aula;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppSpringAulaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
